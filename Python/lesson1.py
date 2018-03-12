f68c262d05364c451e83c4e80bc6af32b9c13d38
guono912 <32875133+guono912@users.noreply.github.com>
2018-03-12 22:22:05 +0800

    Update lesson1.py

--- Python/lesson1.py
+++ Python/lesson1.py
@@ -5,6 +5,7 @@ print('hellow world')
 多行注释为三个引号，类似于Pascal的‘{}’
 另外python每句结尾不需要分号，相对的，对缩进的标准有病态般的追求2333\
 '''
+############################################
 a='world'
 print('hellow ',a)
 #>>>hellow world
@@ -19,4 +20,14 @@ print('''hellow
          world''')
 #并且可以把函数直接放到print中（Pascal没试过）
 print('1+1=',1+1)
-#然后是疯狂类比read/readln的input
+############################################
+#作用和read一样然而差远了的input
+input()  #=>>readln
+#xxx(被赋值)=input('提示语')
+name = input('your name is?/n')
+#默认输入为字符串
+a = input('a=')
+#>>>3
+type(a)
+#<type 'str'>
+a = int(input('a='))  #此时输入为整形，类似的操作有float等
