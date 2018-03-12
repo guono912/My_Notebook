7537450cdb0618062a428b7ffc3018828ca5fbb0
guono912 <32875133+guono912@users.noreply.github.com>
2018-03-12 17:34:55 +0800

    Update lesson1.py

--- Python/lesson1.py
+++ Python/lesson1.py
@@ -1,1 +1,16 @@
-
+#hellow world(#表示注释，类似于//)
+print('hellow world')
+#>>>hellow world
+'''
+多行注释为三个引号，类似于Pascal的‘{}’
+另外python每句结尾不需要分号，相对的，对缩进的标准有病态般的追求2333\
+'''
+a='world'
+print('hellow ',a)
+#>>>hellow world
+#可见python中print的用法和Pascal的write类似，不过有Pascal没有的\n之类的操作
+print('hellow\nworld')
+'''
+hellow
+world
+'''
